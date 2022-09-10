016f17465bf949bd8bfd3825befef56fc5931364
tungth <tranhoangtung1992@gmail.com>
2022-09-10 16:42:56 +0700

    init

---
+++ BaseObject.cs
@@ -0,0 +1,86 @@
+﻿//------------------------------------------------------
+// 
+// Copyright - (c) - 2014 - Mille Boström 
+//
+// Youtube channel - http://www.speedcoding.net
+//------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pong
+{
+    public class BaseObject
+    {
+        public string Username { get; set; }
+        public bool Kill { get; set; }
+
+        private readonly List<Componentss> _components;
+
+        public BaseObject()
+        {
+            _components = new List<Componentss>();
+            Kill = false;
+        }
+
+        public TComponentType GetComponent<TComponentType>(ComponentType componentType) where TComponentType : Componentss
+        {
+            return _components.Find(c => c.ComponentType == componentType) as TComponentType;
+        }
+
+        public void AddComponent(Componentss component)
+        {
+            _components.Add(component);
+            component.Initialize(this);
+        }
+
+        public void AddComponent(List<Componentss> components)
+        {
+            _components.AddRange(components);
+            foreach (var component in components)
+            {
+                component.Initialize(this);
+            }
+        }
+
+        public void RemoveComponent(Componentss component)
+        {
+            _components.Remove(component);
+
+        }
+
+        public virtual void Update(double gameTime)
+        {
+            foreach (var component in _components)
+            {
+                component.Update(gameTime);
+            }
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var component in _components)
+            {
+                component.Draw(spriteBatch);
+            }
+        }
+
+
+        public void Initialize()
+        {
+            if (_components == null)
+                return;
+            _components.ForEach(c => c.Initialize());
+        }
+
+        public void Uninitialize()
+        {
+            if (_components == null)
+                return;
+            _components.ForEach(c => c.Uninitalize());
+        }
+    }
+}
+
+
+
---
+++ Game1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Pong.Manager;
+
+namespace Pong
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+        private ManagerNetwork _managerNetwork;
+        private ManagerInput _managerInput;
+        private ManagerPlayers _managerPlayers;
+        private ManagerEnemies _managerEnemies;
+
+        
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            _managerNetwork = new ManagerNetwork();
+            _managerInput = new ManagerInput();
+            _managerPlayers = new ManagerPlayers(_managerNetwork);
+            _managerEnemies = new ManagerEnemies(_managerNetwork);
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            //ballTexture = Content.Load<Texture2D>("ball");
+            _managerPlayers.LoadContent(Content);
+            _managerEnemies.LoadContent(Content);
+            _managerNetwork.Start();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            _managerNetwork.Update();
+            _managerInput.Update(gameTime.ElapsedGameTime.Milliseconds);
+            _managerPlayers.Update(gameTime.ElapsedGameTime.Milliseconds);
+            _managerEnemies.Update(gameTime.ElapsedGameTime.Milliseconds);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+            if (_managerNetwork.Active)
+            {
+                _managerPlayers.Draw(_spriteBatch);
+                _managerEnemies.Draw(_spriteBatch);
+            }
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
